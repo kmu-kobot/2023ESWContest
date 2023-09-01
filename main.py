451ec093c73cd37283506b7d7a8abca810913031
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2023-09-01 15:57:44 +0900

    :sparkles: main 파일 생성

---
+++ main.py
@@ -0,0 +1,11 @@
+import cv2
+# Robot 클래스 import
+
+if __name__ == "__main__":
+    # ROBOT 객체 생성
+    robot = Robot()
+    # 미션 수행 함수 실행
+    robot.line_tracing_Final()
+    
+    # main while loop
+    # ceremony 완료할 때까지 반복
