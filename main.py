dc201b7455ec4f64fded64e68da8ac60d28995fc
ChaNeeeeeee <105336883+ChaNeeeeeee@users.noreply.github.com>
2023-09-03 16:44:05 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,9 +18,11 @@ if __name__ == "__main__":
     while True:
         frame = Camera.get_image()
         cv2.imshow('frame', frame)
-        if cv2.waitKey(1) & 0xFF == 27:
+        if cv2.waitKey(16) & 0xFF == 27:
             break
-    cv2.destroyAllWindows
+        else:
+            continue
+    cv2.destroyAllWindows()
     
     # start = time.time()
     # end = start
