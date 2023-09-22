f54d423b1cc1dff9bfac5dd9b6057c791b04fa85
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2023-09-22 14:44:29 +0900

    feat: 공 bounding box 위치에 따라 목 상하 조절

--- main.py
+++ main.py
@@ -26,6 +26,8 @@ if __name__ == "__main__":
 
     # 미션 수행 함수 실행
     while True:
+        xmin = ymin = xmax = ymax = False
+
         frame = Camera.get_image()
 
         # run the YOLO model on the frame
@@ -46,6 +48,22 @@ if __name__ == "__main__":
             xmin, ymin, xmax, ymax = int(data[0]), int(data[1]), int(data[2]), int(data[3])
             cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), GREEN, 2)
 
+        # 공 bounding box에 따라 목 각도 조절
+        if ymin == False:    # 공 bounding box가 없다면
+            pass
+        elif ymin < 100:     # 공 bounding box가 위에 있다면 고개 올리기
+            if Robot.neck_pitch < 95:
+                Motion.neckup(1)
+                Robot.neck_pitch -= 5
+        elif ymin > 350:     # 공 bounding box가 아래에 있다면 고개 내리기
+            if 65 < Robot.neck_pitch < 80:
+                Motion.neck65()
+                Robot.neck_pitch = 65
+            elif Robot.neck_pitch > 80:
+                Motion.neck80()
+                Robot.neck_pitch = 80
+        Robot.robot_ball_distance = ball_distance(Robot.neck_pitch)
+        print(f"로봇 목 각도:{Robot.neck_pitch} | 로봇과공:{Robot.robot_ball_distance}")
         # show the frame to our screen
         cv2.imshow("Frame", frame)
         if cv2.waitKey(1600) == ord("q"):
