f1f4aae5e3918227a3a3cf07ee1728b0b4c40857
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2023-09-22 15:07:06 +0900

    feat: 목 상하 80일 때 공 거리 계산 함수 추가

--- Brain/Robot_ball_distance.py
+++ Brain/Robot_ball_distance.py
@@ -2,11 +2,14 @@ def ball_distance(neck_angle, pixel):
     if neck_angle == 65:
         return 0.000372 * (pixel ** 2) - 0.36 * pixel + 105
     elif neck_angle == 80:
-        return (0.0466 * pixel) ** 2 - 9.59 * pixel + 675
+        return 0.000726 * (pixel ** 2) - 0.7 * pixel + 197
     elif neck_angle == 100:
         return 0.00126 * (pixel **2) - 1.29 * pixel + 375
     
 if __name__ == "__main__":
     distance = ball_distance(65, 480)
+    print(distance)
+    distance = ball_distance(80, 480)
+    print(distance)
     distance = ball_distance(100, 480)
     print(distance)
