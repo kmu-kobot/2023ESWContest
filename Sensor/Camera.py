a414d2a16b8ba3347dc22602f29c1d705b0565f8
SeoooooNyeong <113419106+SeoooooNyeong@users.noreply.github.com>
2023-09-25 21:06:17 +0900

    Update Camera.py
    add hole detection using SimepleBlobDetector

--- Sensor/Camera.py
+++ Sensor/Camera.py
@@ -91,6 +91,53 @@ class Camera:
         result = self.model.predict(img, conf = 0.8)[0]
         return result
 
+    def is_hole(self,img):
+        
+        # Setup BlobDetector
+        detector = cv2.SimpleBlobDetector_create()
+        params = cv2.SimpleBlobDetector_Params()
+            
+        # Filter by Area.
+        params.filterByArea = True
+        params.minArea = 9000 #20000
+        params.maxArea = 50000 #40000
+            
+        # Filter by Circularity
+        params.filterByCircularity = True
+        params.minCircularity = 0.05 #0.5
+        
+        # Filter by Convexity
+        params.filterByConvexity = False
+        #params.minConvexity = 0.87
+            
+        # Filter by Inertia
+        params.filterByInertia = True
+        params.minInertiaRatio = 0.1 #0.8
+
+        # Distance Between Blobs
+        params.minDistBetweenBlobs = 200 #200
+            
+        # Create a detector with the parameters
+        detector = cv2.SimpleBlobDetector_create(params)
+
+        while self.cam.isOpened():
+            
+            retval, im = self.cam.read()
+            overlay = im.copy()
+
+            keypoints = detector.detect(im)
+            for k in keypoints:
+                cv2.circle(overlay, (int(k.pt[0]), int(k.pt[1])), int(k.size/2), (0, 0, 255), -1)
+                cv2.line(overlay, (int(k.pt[0])-20, int(k.pt[1])), (int(k.pt[0])+20, int(k.pt[1])), (0,0,0), 3)
+                cv2.line(overlay, (int(k.pt[0]), int(k.pt[1])-20), (int(k.pt[0]), int(k.pt[1])+20), (0,0,0), 3)
+
+            opacity = 0.5
+            cv2.addWeighted(overlay, opacity, im, 1 - opacity, 0, im)
+
+            # Uncomment to resize to fit output window if needed
+            #im = cv2.resize(im, None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+            cv2.imshow("Output", im)
+
 if __name__ == "__main__":
     camera = Camera()
     while True:
