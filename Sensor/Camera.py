b8691230f6639dd33581111d7591de0658174d6d
SeoooooNyeong <113419106+SeoooooNyeong@users.noreply.github.com>
2023-09-25 20:58:04 +0900

    Update Camera.py
    hole detection using SimepleBlobDetector

--- Sensor/Camera.py
+++ Sensor/Camera.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import time
+import sys
 from imutils.video import WebcamVideoStream
 from imutils.video import FPS
 
@@ -30,3 +31,57 @@ class Camera:
     
     def ball_distance(self, angle):
         return 0
+    
+    def is_hole(self,img):
+        
+        # Setup BlobDetector
+        detector = cv2.SimpleBlobDetector_create()
+        params = cv2.SimpleBlobDetector_Params()
+            
+        # Filter by Area.
+        params.filterByArea = True
+        params.minArea = 9000 #20000
+        params.maxArea = 50000 #40000
+            
+        # Filter by Circularity
+        params.filterByCircularity = True
+        params.minCircularity = 0.05 #0.5
+        
+        # Filter by Convexity
+        params.filterByConvexity = False
+        #params.minConvexity = 0.87
+            
+        # Filter by Inertia
+        params.filterByInertia = True
+        params.minInertiaRatio = 0.1 #0.8
+
+        # Distance Between Blobs
+        params.minDistBetweenBlobs = 200 #200
+            
+        # Create a detector with the parameters
+        detector = cv2.SimpleBlobDetector_create(params)
+
+        while self.cam.isOpened():
+            
+            retval, im = self.cam.read()
+            overlay = im.copy()
+
+            keypoints = detector.detect(im)
+            for k in keypoints:
+                cv2.circle(overlay, (int(k.pt[0]), int(k.pt[1])), int(k.size/2), (0, 0, 255), -1)
+                cv2.line(overlay, (int(k.pt[0])-20, int(k.pt[1])), (int(k.pt[0])+20, int(k.pt[1])), (0,0,0), 3)
+                cv2.line(overlay, (int(k.pt[0]), int(k.pt[1])-20), (int(k.pt[0]), int(k.pt[1])+20), (0,0,0), 3)
+
+            opacity = 0.5
+            cv2.addWeighted(overlay, opacity, im, 1 - opacity, 0, im)
+
+            # Uncomment to resize to fit output window if needed
+            #im = cv2.resize(im, None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+            cv2.imshow("Output", im)
+
+            k = cv2.waitKey(1) & 0xff
+            if k == 27:
+                break
+
+        self.cam.release()
+        cv2.destroyAllWindows()
