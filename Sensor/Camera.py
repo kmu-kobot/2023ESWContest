2b1317fdf1dc7db3ec4ca0cf5c87bb8955d49dc3
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2023-09-25 21:12:06 +0900

    feat: hole 감지 opencv 라이브러리 사용

--- Sensor/Camera.py
+++ Sensor/Camera.py
@@ -120,33 +120,27 @@ class Camera:
         # Create a detector with the parameters
         detector = cv2.SimpleBlobDetector_create(params)
 
-        while self.cam.isOpened():
-            
-            retval, im = self.cam.read()
-            overlay = im.copy()
+        overlay = img.copy()
 
-            keypoints = detector.detect(im)
-            for k in keypoints:
-                cv2.circle(overlay, (int(k.pt[0]), int(k.pt[1])), int(k.size/2), (0, 0, 255), -1)
-                cv2.line(overlay, (int(k.pt[0])-20, int(k.pt[1])), (int(k.pt[0])+20, int(k.pt[1])), (0,0,0), 3)
-                cv2.line(overlay, (int(k.pt[0]), int(k.pt[1])-20), (int(k.pt[0]), int(k.pt[1])+20), (0,0,0), 3)
+        keypoints = detector.detect(img)
+        for k in keypoints:
+            cv2.circle(overlay, (int(k.pt[0]), int(k.pt[1])), int(k.size/2), (0, 0, 255), -1)
+            cv2.line(overlay, (int(k.pt[0])-20, int(k.pt[1])), (int(k.pt[0])+20, int(k.pt[1])), (0,0,0), 3)
+            cv2.line(overlay, (int(k.pt[0]), int(k.pt[1])-20), (int(k.pt[0]), int(k.pt[1])+20), (0,0,0), 3)
 
-            opacity = 0.5
-            cv2.addWeighted(overlay, opacity, im, 1 - opacity, 0, im)
+        opacity = 0.5
+        cv2.addWeighted(overlay, opacity, img, 1 - opacity, 0, img)
 
-            # Uncomment to resize to fit output window if needed
-            #im = cv2.resize(im, None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
-            cv2.imshow("Output", im)
+        # Uncomment to resize to fit output window if needed
+        #im = cv2.resize(im, None,fx=0.5, fy=0.5, interpolation = cv2.INTER_CUBIC)
+        cv2.imshow("Output", img)
 
 if __name__ == "__main__":
     camera = Camera()
     while True:
         frame = camera.get_image()
 
-        ret, rect = camera.is_bunker(frame.copy())
-        if ret:
-            cv2.rectangle(frame, (rect[0], rect[1]), (rect[0]+rect[2], rect[1]+rect[3]), (0,255,255), 2)
-        cv2.imshow("Frame", frame)
-        if cv2.waitKey(16) == ord("q"):
+        camera.is_hole(frame.copy())
+        if cv2.waitKey(1600) == ord("q"):
             break
     cv2.destroyAllWindows()
