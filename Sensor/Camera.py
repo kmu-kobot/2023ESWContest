1b7575e7ff00da0dde6d9e02ff05e588918cf7a4
ChaNeeeeeee <105336883+ChaNeeeeeee@users.noreply.github.com>
2023-10-12 07:48:48 +0900

    YOLO delete
    nomore using YOLO

--- Sensor/Camera.py
+++ Sensor/Camera.py
@@ -1,24 +1,10 @@
 import cv2
 import numpy as np
-import torch
-import model.detector
 import utils.utils
 import time
 import math
-# import threading
 from imutils.video import WebcamVideoStream
 from imutils.video import FPS
-
-# class CameraBufferCleanerThread(threading.Thread):
-#     def __init__(self, camera, name='camera-buffer-cleaner-thread'):
-#         self.camera = camera
-#         self.last_frame = None
-#         super(CameraBufferCleanerThread, self).__init__(name=name)
-#         self.start()
-
-#     def run(self):
-#         while True:
-#             ret, self.last_frame = self.camera.read()
 
 class Camera:
     def __init__(self):
@@ -31,18 +17,6 @@ class Camera:
         print(shape) # 세로, 가로 출력
         time.sleep(2)
 
-        # YOLO 설정
-        # if torch.cuda.is_available():
-        #     print("cuda is available")
-        # else:
-        #     print("use cpu")
-        # self.optData = "./Sensor/data/coco.data"
-        # self.optWeight = "./Sensor/weights/ESWv6-290-epoch-0.924759ap-model.pth"
-        # self.cfg = utils.utils.load_datafile(self.optData)
-        # self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        # self.model = model.detector.Detector(self.cfg["classes"], self.cfg["anchor_num"], True).to(self.device)
-        # self.model.load_state_dict(torch.load(self.optWeight, map_location=self.device))
-        # self.model.eval()
     
     # 이미지 공급 쓰레드에서 이미지 하나 get.    
     def get_image(self):
@@ -52,10 +26,7 @@ class Camera:
         except AttributeError: # 이미지를 얻지 못할경우 검은화면 반환
             print("Attribute Error")
             return np.zeros(shape=(480, 640, 3), dtype="uint8")
-        # if self.cam_cleaner.last_frame is not None:
-        #     return self.cam_cleaner.last_frame
-        # else:
-        #     return np.zeros(shape=(480, 640, 3), dtype="uint8")
+
     
     # 홀 인식
     def is_hole(self, img):
@@ -114,80 +85,7 @@ class Camera:
                 return True, [int(x-radius), int(y-radius), int(x+radius), int(y+radius)]
         return False, [False, False, False, False]
     
-    def yoloDetect(self, ori_img): # output is boxed img
-        res_img = cv2.resize(ori_img, (self.cfg["width"], self.cfg["height"]), interpolation = cv2.INTER_LINEAR) 
-        img = res_img.reshape(1, self.cfg["height"], self.cfg["width"], 3)
-        img = torch.from_numpy(img.transpose(0,3, 1, 2))
-        img = img.to(self.device).float() / 255.0
-        
-        preds = self.model(img)
-        
-        output = utils.utils.handel_preds(preds, self.cfg, self.device)
-        output_boxes = utils.utils.non_max_suppression(output, conf_thres = 0.8, iou_thres = 0.5)
-        
-        LABEL_NAMES = []
-        with open(self.cfg["names"], 'r') as f:
-            for line in f.readlines():
-                LABEL_NAMES.append(line.strip())
-        h, w, _ = ori_img.shape
-        scale_h, scale_w = h / self.cfg["height"], w / self.cfg["width"]
-        
-        for box in output_boxes[0]:
-            box = box.tolist()       
-            obj_score = box[4]
-            category = LABEL_NAMES[int(box[5])]
-
-            x1, y1 = int(box[0] * scale_w), int(box[1] * scale_h)
-            x2, y2 = int(box[2] * scale_w), int(box[3] * scale_h)
-
-            cv2.rectangle(ori_img, (x1, y1), (x2, y2), (255, 255, 0), 2)
-            cv2.putText(ori_img, '%.2f' % obj_score, (x1, y1 - 5), 0, 0.7, (0, 255, 0), 2)	
-            cv2.putText(ori_img, category, (x1, y1 - 25), 0, 0.7, (0, 255, 0), 2)
-            return True, [x1, y1, x2, y2], ori_img
-        return False, [False,False,False,False] , ori_img
-
-    def yoloDetect_master(self, ori_img):
-        res_img = cv2.resize(ori_img, (self.cfg["width"], self.cfg["height"]), interpolation = cv2.INTER_LINEAR) 
-        img = res_img.reshape(1, self.cfg["height"], self.cfg["width"], 3)
-        img = torch.from_numpy(img.transpose(0,3, 1, 2))
-        img = img.to(self.device).float() / 255.0
-        
-        preds = self.model(img)
-        
-        output = utils.utils.handel_preds(preds, self.cfg, self.device)
-        output_boxes = utils.utils.non_max_suppression(output, conf_thres = 0.8, iou_thres = 0.5)
-        
-        LABEL_NAMES = []
-        with open(self.cfg["names"], 'r') as f:
-            for line in f.readlines():
-                LABEL_NAMES.append(line.strip())
-        h, w, _ = ori_img.shape
-        scale_h, scale_w = h / self.cfg["height"], w / self.cfg["width"]
-        
-        hole = golfBall = arrow = False
-        holebox = golfBallBox = arrowBox = [False, False, False, False]
-        for box in output_boxes[0]:
-            box = box.tolist()       
-            obj_score = box[4]
-            category = LABEL_NAMES[int(box[5])]
-
-            x1, y1 = int(box[0] * scale_w), int(box[1] * scale_h)
-            x2, y2 = int(box[2] * scale_w), int(box[3] * scale_h)
-            if category == "hall":
-                hole = True
-                holebox = [x1, y1, x2, y2]
-            elif category == "golfBall":
-                golfBall = True
-                golfBallBox = [x1, y1, x2, y2]
-            elif category == "arrow":
-                arrow = True
-                arrowBox = [x1, y1, x2, y2]
-            # cv2.rectangle(ori_img, (x1, y1), (x2, y2), (255, 255, 0), 2)
-            # cv2.putText(ori_img, '%.2f' % obj_score, (x1, y1 - 5), 0, 0.7, (0, 255, 0), 2)	
-            # cv2.putText(ori_img, category, (x1, y1 - 25), 0, 0.7, (0, 255, 0), 2)
-        return hole, holebox, golfBall, golfBallBox, arrow, arrowBox
     
-
 if __name__ == "__main__":
     camera = Camera()
     while True:
