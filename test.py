a5d1d0eaf3c1265503884879ddda914c6646df6f
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2023-10-11 20:03:46 +0900

    feat: 공 인식 안되는 경우 이상치 제거

--- test.py
+++ test.py
@@ -11,6 +11,7 @@ CONFIDENCE_THRESHOLD = 0.8
 GREEN = (0, 255, 0)
 
 shot_count = 0
+plain_frame_count = 0
 
 if __name__ == "__main__":
     
@@ -22,14 +23,20 @@ if __name__ == "__main__":
 
     Motion.initial()
     Motion.init(True)
-    time.sleep(3)
 
-    # 미션 수행 함수 실행 - 반복 한 번에 동작 한 가지만 실행
+    # 미션 수행 함수 실행
+    print("Loop 시작 :)")
     while True:
         frame = Camera.get_image()
 
         # image process
         Robot.is_hole, holeBox, Robot.is_ball, ballBox, Robot.is_arrow, arrowBox = Camera.yoloDetect_master(frame)
+        if Robot.is_ball:
+            cv2.rectangle(frame, (ballBox[0], ballBox[1]), (ballBox[2], ballBox[3]), (0,0,255), 2)
+        if Robot.is_hole:
+            cv2.rectangle(frame, (holeBox[0]. holeBox[1]), (holeBox[2], holeBox[3]), (0,255,255), 2)
+        if Robot.is_arrow:
+            cv2.rectangle(frame, (arrowBox[0], arrowBox[1]), (arrowBox[2], arrowBox[3]), (255,255,255), 2)
 
 
         # Finite State Machine
@@ -45,15 +52,18 @@ if __name__ == "__main__":
         elif Robot.curr_mission == "ApproachBall":
             xmin, ymin, xmax, ymax = ballBox
             Robot.robot_ball_distance = ball_distance(Robot.neck_pitch, ymax)
+            # 공에 가까이 가는 중에 순간 공이 감지되지 않았다면 일단 기다리기
+            if not Robot.is_ball:
+                plain_frame_count += 1
+                if plain_frame_count > 5:
+                    Robot.curr_mission = "FindBall"
+                    plain_frame_count = 0
             # 공이 shot 가능한 위치에 있으면 goal을 찾는다
-            if Robot.is_ball and  12 <= Robot.robot_ball_distance <= 13:
+            elif Robot.is_ball and  12 <= Robot.robot_ball_distance <= 13:
                 Robot.curr_mission == "FindGoal"
             # 공이 shot 불가능한 위치에 있으면 공으로 다가간다
-            elif Robot.is_ball:
-                Robot.curr_mission = "ApproachBall"
-            # 공이 없으면 공을 찾는다 - Approach 동작을 수행하다가 공이 사라진 경우
             else:
-                Robot.curr_mission = "FindBall"
+                Robot.curr_mission = "ApproachBall"
         # 3. FindGoal
         elif Robot.curr_mission == "FindGoal":
             xmin, ymin, xmax, ymax = holeBox
@@ -75,33 +85,41 @@ if __name__ == "__main__":
            
 
         # motion
+        # 1. FindBall
         if Robot.curr_mission == "FindBall":
             Motion.init(True)
             Motion.turn("LEFT", 45)
             Robot.neck_pitch = 100
             Robot.neck_yaw = 0
+        # 2. ApproachBall
         elif Robot.curr_mission == "ApproachBall":
             xmin, ymin, xmax, ymax = ballBox
-            if ymin < 190 and Robot.neck_pitch < 100:
-                Robot.neck_pitch += 5
-                Motion.neckup(Robot.neck_pitch)
-            elif ymax > 290 and Robot.neck_pitch > 35:
-                Robot.neck_pitch -= 5
-                Motion.neckup(Robot.neck_pitch)
-            if xmin < 270:
-                Motion.crab("RIGHT")
-            elif xmax > 370:
-                Motion.crab("LEFT")
-            
-            if Robot.robot_ball_distance > 20:
-                Motion.walk()
-            elif Robot.robot_ball_distance > 13:
-                Motion.step()
-            elif Robot.robot_ball_distance < 12:
-                Motion.step("BACK")
+            # 공에 다가가다가 순간 공이 안 보인 경우
+            if not Robot.is_ball:
+                pass
+            # 공 bounding box가 화면 중앙에 오도록 움직이고 shot 가능할때까지 걸어간다
             else:
-                Motion.init()
-            Robot.neck_yaw = 0
+                if ymin < 190 and Robot.neck_pitch < 100:
+                    Robot.neck_pitch += 5
+                    Motion.neckup(Robot.neck_pitch)
+                elif ymax > 290 and Robot.neck_pitch > 35:
+                    Robot.neck_pitch -= 5
+                    Motion.neckup(Robot.neck_pitch)
+                if xmin < 270:
+                    Motion.crab("RIGHT")
+                elif xmax > 370:
+                    Motion.crab("LEFT")
+                
+                if Robot.robot_ball_distance > 15:
+                    Motion.walk()
+                elif Robot.robot_ball_distance > 13:
+                    Motion.step()
+                elif Robot.robot_ball_distance < 12:
+                    Motion.step("BACK")
+                else:
+                    Motion.init()
+                Robot.neck_yaw = 0
+        # 3. FindGoal
         elif Robot.curr_mission == "FindGoal":
             if Robot.neck_yaw == 0:
                 Robot.neck_yaw = -90
@@ -109,11 +127,13 @@ if __name__ == "__main__":
             elif Robot.neck_yaw == -90:
                 Robot.neck_yaw = 0
                 Motion.view(0)
+        # 4. ApproachGoal
         elif Robot.curr_mission == "ApproachGoal":
             Robot.neck_pitch = 100
             Robot.neck_yaw = 0
             Motion.init(True)
-            # Motion.turn() 홀 찾아 원 궤도로 회전하는 동작
+            # TODO Motion.turn() 홀 찾아 원 궤도로 회전하는 동작
+        # 5. Shot
         elif Robot.curr_mission == "Shot":
             Robot.neck_pitch = 100
             Robot.neck_yaw = 0
