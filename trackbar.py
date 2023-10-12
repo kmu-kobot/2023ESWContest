57dfd3dc5c799212553bb8eab52914fd9faa6b5e
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2023-10-12 19:15:27 +0900

    fix: trackbar HSV로 변경

--- trackbar.py
+++ trackbar.py
@@ -4,51 +4,43 @@ import numpy as np
 def empty_callback(x):
     pass
 
-# VideoCapture 객체를 생성하여 비디오 파일을 엽니다.
-cap = cv2.VideoCapture(0)  # 비디오 파일의 경로를 지정해주세요
+cap = cv2.VideoCapture(0)
 
-# 창을 생성하여 트랙바를 추가합니다.
 cv2.namedWindow('Video')
 
-# 하한(H, L, S) 값의 초기 설정을 지정합니다.
-h_min, l_min, s_min = 0, 0, 0
-h_max, l_max, s_max = 179, 255, 255
+h_min, s_min, v_min = 0, 0, 0
+h_max, s_max, v_max = 179, 255, 255
 
 cv2.createTrackbar('Hue Min', 'Video', h_min, 179, empty_callback)
-cv2.createTrackbar('Lightness Min', 'Video', l_min, 255, empty_callback)
 cv2.createTrackbar('Saturation Min', 'Video', s_min, 255, empty_callback)
+cv2.createTrackbar('Value Min', 'Video', v_min, 255, empty_callback)
 cv2.createTrackbar('Hue Max', 'Video', h_max, 179, empty_callback)
-cv2.createTrackbar('Lightness Max', 'Video', l_max, 255, empty_callback)
 cv2.createTrackbar('Saturation Max', 'Video', s_max, 255, empty_callback)
+cv2.createTrackbar('Value Max', 'Video', v_max, 255, empty_callback)
 
 while True:
     ret, frame = cap.read()
     if not ret:
         break
 
-    # 트랙바에서 현재 설정된 값들을 읽어옵니다.
     h_min = cv2.getTrackbarPos('Hue Min', 'Video')
-    l_min = cv2.getTrackbarPos('Lightness Min', 'Video')
     s_min = cv2.getTrackbarPos('Saturation Min', 'Video')
+    v_min = cv2.getTrackbarPos('Value Min', 'Video')
     h_max = cv2.getTrackbarPos('Hue Max', 'Video')
-    l_max = cv2.getTrackbarPos('Lightness Max', 'Video')
     s_max = cv2.getTrackbarPos('Saturation Max', 'Video')
+    v_max = cv2.getTrackbarPos('Value Max', 'Video')
 
-    # 영상을 HLS 색상 공간으로 변환합니다.
-    hls = cv2.cvtColor(frame, cv2.COLOR_BGR2HLS)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # 범위 설정을 통해 마스크를 생성합니다.
-    lower_bound = np.array([h_min, l_min, s_min])
-    upper_bound = np.array([h_max, l_max, s_max])
-    mask = cv2.inRange(hls, lower_bound, upper_bound)
+    lower_bound = np.array([h_min, s_min, v_min])
+    upper_bound = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(hsv, lower_bound, upper_bound)
 
-    # 마스크를 사용하여 영상을 필터링합니다.
     filtered_frame = cv2.bitwise_and(frame, frame, mask=mask)
 
-    # 화면에 영상을 표시합니다.
     cv2.imshow('Frame', filtered_frame)
 
-    if cv2.waitKey(1) & 0xFF == 27:  # 'Esc' 키를 누르면 종료합니다.
+    if cv2.waitKey(1) & 0xFF == 27:
         break
 
 cap.release()
