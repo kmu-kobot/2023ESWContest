acea9411b8201e975cb06d3dd999f19e38b0a06d
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2023-10-14 14:41:53 +0900

    delete: test 파일 삭제

--- main.py
+++ main.py
@@ -7,10 +7,8 @@ import cv2
 import numpy as np
 import time
 
-CONFIDENCE_THRESHOLD = 0.8
-GREEN = (0, 255, 0)
-
 shot_count = 0
+plain_frame_count = 0
 
 if __name__ == "__main__":
     
@@ -18,95 +16,154 @@ if __name__ == "__main__":
     Motion = Motion()
     Camera = Camera()
     
-    direction = "CENTER"
-
-    # 시리얼 연결 확인 동작
     Motion.initial()
-    # 초기 자세
+    Motion.wait_unlock()
     Motion.init(True)
-    time.sleep(3)
+    Motion.wait_unlock()
+    Motion.neckup(70)
+    Motion.wait_unlock()
 
-    # 미션 수행 함수 실행 - 반복 한 번에 동작 한 가지만 실행
+    # 미션 수행 함수 실행
+    print("Loop 시작 :)")
     while True:
         frame = Camera.get_image()
 
         # image process
-        Robot.is_hole, holeBox, Robot.is_ball, ballBox, Robot.is_arrow, arrowBox = Camera.yoloDetect_master(frame)
+        img = frame.copy()
+        Robot.is_ball, ballBox1, ballBox2 = Camera.hsvDetect(img)
 
-        # 공 인식
-        # 공과의 거리가 12 이하, 13 이상이면 approach ball
-        # 공과의 거리가 12~13이면 공을 칠 목표점을 찾아야 함
-        # 공이 없으면 find ball
-        xmin, ymin, xmax, ymax = ballBox
-        if Robot.is_ball and Robot.neck_yaw == 0:
-            cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), (0,0,255), 2)
-            Robot.robot_ball_distance = ball_distance(Robot.neck_pitch, ymax)
-            if Robot.robot_ball_distance > 13:
-                if not Motion.getRx():
-                    Motion.walk()
-                Robot.curr_mission = "ApproachBall"
-            elif Robot.robot_ball_distance < 12:
-                Motion.step("BACK")
-                Robot.curr_mission = "ApproachBall"
-            else:
-                Motion.init()
-                Robot.curr_mission == "FindGoal"
-        # 홀 인식
-        # 홀이 로봇 시선 중앙에 있으면 shot  
-        elif Robot.is_hole and Robot.neck_yaw == -90 and Robot.curr_mission == "FindGoal":
-            cv2.rectangle(frame, (holeBox[0], holeBox[1]), (holeBox[2], holeBox[3]), (255,255,0), 2)
-            if 300 < (holeBox[0] + holeBox[2]) / 2 < 340:
-                Robot.curr_mission = "Shot"
-            else:
-                Robot.curr_mission = "ApproachGoal"
-        elif not Robot.is_ball:
-            Robot.curr_mission = "FindBall"
+        if Robot.is_ball:
+            cv2.rectangle(frame, ballBox1, ballBox2, (0,0,255), 2)
 
-        # motion
-        if Motion.getRx() and Robot.curr_mission != "ApproachBall":
-            pass
-        # shot 가능한 거리라면 hole을 찾기 위해 목을 돌림
-        elif Robot.curr_mission == "FindGoal":
-            if Robot.neck_yaw == 0:
-                Robot.neck_yaw = -90
-                Motion.view(-90)
-            elif Robot.neck_yaw == -90:
-                Robot.neck_yaw = 0
-                Motion.view(0)
-        # 공이 감지된 후 공으로 다가감
+        # Finite State Machine
+        # 1. FindBall
+        if Robot.curr_mission == "FindBall":
+            # 공이 있으면 공으로 다가간다
+            if Robot.is_ball:
+                Robot.curr_mission = "ApproachBall"
+            # 공이 없으면 계속 공을 찾는다
+            else:
+                Robot.curr_mission = "FindBall"
+        # 2. ApproachBall
         elif Robot.curr_mission == "ApproachBall":
-            if ymin < 190:
-                if Robot.neck_pitch < 100:
-                    Robot.neck_pitch += 5
-                    Motion.neckup(Robot.neck_pitch)
-            elif ymax > 290:
-                if Robot.neck_pitch > 35:
-                    Robot.neck_pitch -= 5
-                    Motion.neckup(Robot.neck_pitch)
-            if xmin < 270:
-                Motion.crab("RIGHT")
-            elif xmax > 370:
-                Motion.crab("LEFT")
-        # 공과 충분히 가까워진 후 샷
-        elif Robot.curr_mission == "Shot":
-            print("Shot!!")
-            Motion.view(0)
-            Motion.shot()
-            time.sleep(10)
-            Motion.init(True)
-            Robot.neck_pitch = 100
-            Robot.neck_yaw = 0
-        elif Robot.curr_mission == "FindBall":
-            Motion.init(True)
-            Motion.turn("LEFT", 45)
-        elif Robot.curr_mission == "ApproachGoal":
-            # 회전 보행
-            pass
-        
+            (xmin, ymin) = ballBox1
+            (xmax, ymax) = ballBox2
+            Robot.robot_ball_distance = ball_distance(Robot.neck_pitch, ymax)
+            print(f"공으로 다가가는 중: {int(Robot.robot_ball_distance)} cm")
+            # 공에 가까이 가는 중에 순간 공이 감지되지 않았다면 일단 기다린다
+            if not Robot.is_ball:
+                plain_frame_count += 1
+                if plain_frame_count > 5:
+                    Robot.curr_mission = "FindBall"
+                    plain_frame_count = 0
+            # 공이 shot 가능한 위치에 있으면 goal을 찾는다
+            elif Robot.is_ball and  12 <= Robot.robot_ball_distance <= 15:
+                Robot.curr_mission = "FindGoal"
+                plain_frame_count = 0
+            # 공이 shot 불가능한 위치에 있으면 공으로 다가간다
+            else:
+                Robot.curr_mission = "ApproachBall"
+                plain_frame_count = 0
+          
+        print(f"현재 상태 {Robot.curr_mission}, neck: {Robot.neck_pitch}")
+
 
         # show the frame to our screen
         cv2.imshow("Frame", frame)
-        if cv2.waitKey(16) == ord("q"):
+        if cv2.waitKey(1) == ord("q"):
             break
+
+
+        # motion
+        # 1. FindBall
+        if Motion.getRx() and Robot.curr_mission != "ApproachBall":
+            pass
+        elif Robot.curr_mission == "FindBall":
+            Motion.init()
+            Motion.wait_unlock()
+            Motion.turn("LEFT", 45)
+            Motion.wait_unlock()
+            time.sleep(2)
+            Robot.neck_yaw = 0
+        # 2. ApproachBall
+        elif Robot.curr_mission == "ApproachBall":
+            (xmin, ymin) = ballBox1
+            (xmax, ymax) = ballBox2
+            xmean = (xmin + xmax) // 2
+            ymean = (ymin + ymax) // 2
+            # 공에 다가가다가 순간 공이 안 보인 경우
+            if not Robot.is_ball:
+                pass
+            # 공 bounding box가 화면 중앙에 오도록 움직이고 shot 가능할때까지 걸어간다
+            else:
+                if xmean < 160:
+                    if Motion.getRx():
+                        Motion.init()
+                        Motion.wait_unlock()
+                    Motion.turn("LEFT", 20)
+                    Motion.wait_unlock()
+                    print("TURN LEFT")
+                elif xmean > 480:
+                    if Motion.getRx():
+                        Motion.init()
+                        Motion.wait_unlock()
+                    Motion.turn("RIGHT", 20)
+                    Motion.wait_unlock()
+                    print("TURN RIGHT")
+                elif ymean < 100 and Robot.neck_pitch < 100:
+                    if Motion.getRx():
+                        Motion.init()
+                        Motion.wait_unlock() 
+                    Robot.neck_pitch += 5
+                    Motion.neckup(Robot.neck_pitch)
+                    Motion.wait_unlock()
+                    print("목 조절")
+                elif ymean > 380 and Robot.neck_pitch > 35:
+                    if Motion.getRx():
+                        Motion.init()
+                        Motion.wait_unlock()
+                    Robot.neck_pitch -= 5
+                    Motion.neckup(Robot.neck_pitch)
+                    Motion.wait_unlock()
+                    print("목 조절")
+                elif Robot.robot_ball_distance > 30:
+                    Motion.walk()
+                    print("WALKING")
+                elif Robot.robot_ball_distance > 15:
+                    if Motion.getRx():
+                        Motion.init()
+                        Motion.wait_unlock()
+                    Motion.step()
+                    Motion.wait_unlock()
+                elif xmean < 300:
+                    if Motion.getRx():
+                        Motion.init()
+                        Motion.wait_unlock()
+                    Motion.turn("LEFT")
+                    Motion.wait_unlock()
+                    print("TURN LEFT")
+                elif xmean > 340:
+                    if Motion.getRx():
+                        Motion.init()
+                        Motion.wait_unlock()
+                    Motion.turn("RIGHT")
+                    Motion.wait_unlock()
+                    print("TURN RIGHT")
+                elif Robot.robot_ball_distance < 12:
+                    if Motion.getRx():
+                        Motion.init()
+                        Motion.wait_unlock()
+                    Motion.step("BACK")
+                    Motion.wait_unlock()
+                else:
+                    Motion.init()
+                    Motion.wait_unlock()
+                Robot.neck_yaw = 0
+        elif Robot.curr_mission == "FindGoal":
+            Motion.initial()
+            print("Shot!!!!!!!!!!!!!!!!!!!!!!!!!")
+            time.sleep(10)
+            # TODO shot 이후에 목 각도 70으로
+
         
     cv2.destroyAllWindows()
