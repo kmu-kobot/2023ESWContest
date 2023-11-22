da7d25409500713b92ad6232d1c93b9d7a035f53
ChaNeeeeeee <105336883+ChaNeeeeeee@users.noreply.github.com>
2023-11-22 19:45:28 +0900

    Update mouseclickevent.py

--- mouseclickevent.py
+++ mouseclickevent.py
@@ -1,25 +1,31 @@
+from Actuator.Motion import Motion
+from Sensor.Camera import Camera
+from Brain.Robot import Robot
+
 import cv2
 
 def mouse_callback(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         print(f'({x}, {y})')
 
-cap = cv2.VideoCapture(0)
-
+Robot = Robot()
+Motion = Motion()
+Camera = Camera()
+    
 cv2.namedWindow('frame')
 cv2.setMouseCallback('frame', mouse_callback)
 
 while True:
-    ret, frame = cap.read()
+    frame = Camera.get_image()
 
-    if not ret:
-        break
-
+    cv2.line(frame, (325,0), (522,480), (0,0,255), 2)
+    cv2.line(frame, (335,0), (595,480), (255,0,0), 2)
+    cv2.line(frame, (313,0), (447,480), (255,0,0), 2)
+        
     cv2.imshow('frame', frame)
 
     key = cv2.waitKey(1)
     if key == 27:
         break
 
-cap.release()
 cv2.destroyAllWindows()
