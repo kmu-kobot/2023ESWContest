be11983def327cc90c7b152ed4415e88bd616470
ChaNeeeeeee <105336883+ChaNeeeeeee@users.noreply.github.com>
2023-11-23 17:06:48 +0900

    long shot upgrade

--- mouseclickevent.py
+++ mouseclickevent.py
@@ -1,31 +1,44 @@
-from Actuator.Motion import Motion
-from Sensor.Camera import Camera
-from Brain.Robot import Robot
+# from Actuator.Motion import Motion
+# from Sensor.Camera import Camera
+# from Brain.Robot import Robot
 
 import cv2
+import numpy
 
 def mouse_callback(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         print(f'({x}, {y})')
 
-Robot = Robot()
-Motion = Motion()
-Camera = Camera()
+# Robot = Robot()
+# Motion = Motion()
+# Camera = Camera()
 
-Motion.initial()
-Motion.init(True)
-Motion.neckup(70)
-Motion.view(-90)
+# Motion.initial()
+# Motion.init(True)
+# Motion.neckup(70)
+# Motion.view(-90)
     
 cv2.namedWindow('frame')
 cv2.setMouseCallback('frame', mouse_callback)
 
 while True:
-    frame = Camera.get_image()
+    frame = numpy.zeros(shape=(480, 640, 3), dtype="uint8")
 
-    cv2.line(frame, (325,0), (522,480), (0,0,255), 2)
-    cv2.line(frame, (335,0), (595,480), (255,0,0), 2)
-    cv2.line(frame, (313,0), (447,480), (255,0,0), 2)
+    cv2.line(frame, (325,0), (522,480), (0,0,255), 1)
+    cv2.line(frame, (333,0), (580,480), (255,0,0), 1)
+    cv2.line(frame, (315,0), (460,480), (255,0,0), 1)
+    
+    # 5 degree
+    cv2.line(frame, (360,0), (522,480), (200,200,0), 1)
+    cv2.line(frame, (295,0), (522,480), (200,200,0), 1)
+    
+    # 10 degree
+    cv2.line(frame, (410,0), (522,480), (0,200,200), 1)
+    cv2.line(frame, (253,0), (522,480), (0,200,200), 1)
+    
+    # 20 degree
+    cv2.line(frame, (500,0), (522,480), (200,0,200), 1)
+    cv2.line(frame, (170,0), (522,480), (200,0,200), 1)
         
     cv2.imshow('frame', frame)
 
