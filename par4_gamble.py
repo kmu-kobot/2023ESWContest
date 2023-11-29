1ddb0613484906c9a5127b63b86dd60d6b9a1649
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2023-11-29 21:06:57 +0900

    fix: 두번째 샷까지는 longchecker_far

--- par4_gamble.py
+++ par4_gamble.py
@@ -113,7 +113,11 @@ if __name__ == "__main__":
                 Robot.curr_mission = "Ceremony"            
         # 4. LongCheck
         elif Robot.curr_mission == "LongCheck":
-            Robot.shotzone, frame, shot_power = Camera.longChecker(img)
+            if shot_count < 2:
+                Robot.shotzone, frame, shot_power = Camera.longChecker_far(img)
+            else:
+                Robot.shotzone, frame, shot_power = Camera.longChecker(img)
+                
             if Robot.shotzone == "!!!Shot!!!":
                 if shot_power < 10:
                     Robot.long_shot = False
