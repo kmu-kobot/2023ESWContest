596344e100237cb502dd75164a3587eb37346426
ChaNeeeeeee <105336883+ChaNeeeeeee@users.noreply.github.com>
2023-11-30 20:52:36 +0900

    Update mouseclickevent.py

--- mouseclickevent.py
+++ mouseclickevent.py
@@ -15,8 +15,8 @@ Camera = Camera()
 
 Motion.initial()
 Motion.init(True)
-Motion.neckup(80)
-Motion.view(90)
+Motion.neckup(70)
+Motion.view(-90)
     
 cv2.namedWindow('frame')
 cv2.setMouseCallback('frame', mouse_callback)
@@ -24,21 +24,21 @@ cv2.setMouseCallback('frame', mouse_callback)
 while True:
     frame = Camera.get_image()
 
-    cv2.line(frame, (270,0), (120,480), (0,0,255), 1)
-    cv2.line(frame, (315,0), (185,480), (255,0,0), 1)
-    cv2.line(frame, (230,0), (45,480), (255,0,0), 1)
+    cv2.line(frame, (325,0), (522,480), (0,0,255), 1)
+    cv2.line(frame, (333,0), (580,480), (255,0,0), 1)
+    cv2.line(frame, (253,0), (470,480), (255,0,0), 1)
         
     # 5 degree
-    cv2.line(frame, (350,0), (120,480), (200,200,0), 1)
-    cv2.line(frame, (195,0), (120,480), (200,200,0), 1)
+    cv2.line(frame, (360,0), (522,480), (200,200,0), 1)
+    cv2.line(frame, (295,0), (522,480), (200,200,0), 1)
         
     # 10 degree
-    cv2.line(frame, (400,0), (120,480), (0,200,200), 1)
-    cv2.line(frame, (150,0), (120,480), (0,200,200), 1)
+    cv2.line(frame, (430,0), (522,480), (0,200,200), 1)
+    cv2.line(frame, (253,0), (522,480), (0,200,200), 1)
         
     # 20 degree
-    cv2.line(frame, (480,0), (120,480), (200,0,200), 1)
-    cv2.line(frame, (70,0), (120,480), (200,0,200), 1)
+    cv2.line(frame, (500,0), (522,480), (200,0,200), 1)
+    cv2.line(frame, (150,0), (522,480), (200,0,200), 1)
         
     cv2.imshow('frame', frame)
 
