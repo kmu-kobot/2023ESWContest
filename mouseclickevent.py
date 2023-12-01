d82d9c07847e179e5446a3c7b13e77c3027957a5
ChaNeeeeeee <105336883+ChaNeeeeeee@users.noreply.github.com>
2023-12-01 15:52:45 +0900

    Update mouseclickevent.py

--- mouseclickevent.py
+++ mouseclickevent.py
@@ -1,6 +1,6 @@
-# from Actuator.Motion import Motion
-# from Sensor.Camera import Camera
-# from Brain.Robot import Robot
+from Actuator.Motion import Motion
+from Sensor.Camera import Camera
+from Brain.Robot import Robot
 
 import cv2
 import numpy
@@ -9,36 +9,33 @@ def mouse_callback(event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
         print(f'({x}, {y})')
 
-# Robot = Robot()
-# Motion = Motion()
-# Camera = Camera()
+Robot = Robot()
+Motion = Motion()
+Camera = Camera()
 
-# Motion.initial()
-# Motion.init(True)
-# Motion.neckup(70)
-# Motion.view(-90)
+Motion.initial()
+Motion.init(True)
+Motion.neckup(80)
+Motion.view(-90)
     
 cv2.namedWindow('frame')
 cv2.setMouseCallback('frame', mouse_callback)
 
 while True:
-    frame = numpy.zeros(shape=(480, 640, 3), dtype="uint8")
+    img = Camera.get_image()
 
-    cv2.line(frame, (290,0), (522,480), (0,0,255), 1)
-    cv2.line(frame, (325,0), (522,480), (255,0,0), 1)
-    cv2.line(frame, (253,0), (470,480), (255,0,0), 1)
-    
+    cv2.line(img, (350,0), (120,480), (0,0,255), 1)
+    cv2.line(img, (360,0), (150,480), (255,0,0), 1)
+    cv2.line(img, (340,0), (90,480), (255,0,0), 1)
     # 5 degree
-    cv2.line(frame, (360,0), (522,480), (200,200,0), 1)
-    cv2.line(frame, (220,0), (522,480), (200,200,0), 1)
-    
+    cv2.line(img, (390,0), (120,480), (200,200,0), 1)
+    cv2.line(img, (310,0), (120,480), (200,200,0), 1)
     # 10 degree
-    cv2.line(frame, (430,0), (522,480), (0,200,200), 1)
-    # cv2.line(frame, (253,0), (522,480), (0,200,200), 1)
-    
+    cv2.line(img, (460,0), (120,480), (0,200,200), 1)
+    cv2.line(img, (240,0), (120,480), (0,200,200), 1)
     # 20 degree
-    cv2.line(frame, (500,0), (522,480), (200,0,200), 1)
-    cv2.line(frame, (150,0), (522,480), (200,0,200), 1)
+    cv2.line(img, (520,0), (120,480), (200,0,200), 1)
+    cv2.line(img, (180,0), (120,480), (200,0,200), 1)
         
     cv2.imshow('frame', frame)
 
